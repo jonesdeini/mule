73dc0dd8cf50c66d784ac8c2f79ecd4939245a2d
jonesdeini <jonesdeini@gmail.com>
2013-06-28 22:51:06 -0400

    readability

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -4,13 +4,11 @@ import (
   "fmt"
   "io/ioutil"
   "net/http"
-  "github.com/jonesdeini/gokogiri"
-  /* "github.com/jonesdeini/gokogiri/xml" */
+  "github.com/moovweb/gokogiri"
 )
 
 func ScrapeAllTheThings(url string) {
   pageSource := retrievePageSource("http://www.sc2ratings.com/season-info.php?season=spl2&section=Round%206")
-  /* highest_container, err := scrape_highest_container(pageSource) */
   scrape_highest_container(pageSource)
 }
 
@@ -23,15 +21,11 @@ func retrievePageSource(url string) []byte {
   return body
 }
 
-/* func scrape_highest_container(pageSource []byte) ([]xml.Node, error) { */
 func scrape_highest_container(pageSource []byte) {
   doc, err := gokogiri.ParseHtml(pageSource)
   errorHandler(err)
   defer doc.Free()
-  // cannot use []"github.com/moovweb/gokogiri/xml".Node as type []"github.com/jonesdeini/gokogiri/xml".Node in return argument
-  /* return doc.Search(".//*[@class='season-round-date-container']") */
 
-  // scrape_children_of_highest_container(highest_container []xml.Node)
   highest_container, err := doc.Search(".//*[@class='season-round-date-container']")
   for i := range highest_container {
     headline, err := highest_container[i].Search(".//*[@class='headline']")
@@ -45,16 +39,3 @@ func scrape_highest_container(pageSource []byte) {
   }
 
 }
-
-/* func scrape_children_of_highest_container(highest_container []xml.Node) { */
-/*   for i := range highest_container { */
-/*     headline, err := highest_container[i].Search(".//*[@class='headline']") */
-/*     errorHandler(err) */
-
-/*     subhead, err := highest_container[i].Search(".//*[@class='sub-head']") */
-/*     errorHandler(err) */
-
-/*     fmt.Println(headline) */
-/*     fmt.Println(subhead) */
-/*   } */
-/* } */
